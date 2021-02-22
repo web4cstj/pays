96e76984ed6c0e27acb1089f14cd6a0c64b96f9b
Martin Boudreau <32491185+bobanum@users.noreply.github.com>
2021-02-22 06:35:25 -0500

    Départ

--- index.php
+++ index.php
@@ -1,23 +1,37 @@
 <?php
-include('Directive.class.php');
+// La connexion
+
+// Les variables
+
+// La composition du SQL
+
+// L'exécution de la requête
+
+//La composition de l'affichage
+$affichage = '';
+
 ?><!DOCTYPE html>
 <html lang="fr">
 <head>
-<title>Exercice pas à pas</title>
-<meta charset="utf-8" />
-<link rel="stylesheet" href="pagestatique.css" media="all" />
-<link rel="stylesheet" href="directives.css" media="all" />
+<meta charset="UTF-8"/>
+<link rel="stylesheet" href="pays.css"/>
+<title>Affichage des pays</title>
 </head>
 
 <body>
 <div class="interface">
-	<input type="checkbox" class="afficher" name="afficher" id="afficher_tout"/><label for="afficher_tout" title="Afficher toutes les solutions">⚑</label>
 	<header>
-		<h1>Exercice pas à pas</h1>
+		<h1>Affichage des pays</h1>
 	</header>
-	<section class="body">
-	<?php echo $affichage; ?>
-	</section>
+	<div class="body">
+		<?php
+		include "menu.inc.php";
+		echo menu();
+
+		// L'affichage du tableau
+		echo $affichage;
+		?>
+	</div>
 </div>
 </body>
 </html>
